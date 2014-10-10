6c07ca501105dd4a69257d2affc7d22b96f46b0e
Sassar <ta1.sakura96@gmail.com>
2014-10-10 15:50:01 +0900

    1stCommit
    最初のコミット。

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	
+	public float screenW = Screen.width;
+	public float screenH = Screen.height;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Application.platform == RuntimePlatform.Android && Input.GetKey(KeyCode.Escape)) {
+			Application.Quit ();
+		}
+	}
+	void OnGUI(){
+			//StartButton
+			if (GUI.Button (new Rect (screenW / 2, screenH / 4, 125, 75), "Start")) {
+					Application.LoadLevel(1);
+			}
+
+			//QuitButton
+			if (GUI.Button (new Rect (screenW / 2, screenH / 4 + 100, 125, 75), "Quit")) {
+					Application.Quit ();
+			}
+	}
+}
